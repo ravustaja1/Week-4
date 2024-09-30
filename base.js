2b2f0cd0aad95f20b8d0635e6eb9b5538c261572
Leo Solehmainen <leo.solehmainen@student.lut.fi>
2024-09-30 13:14:49 +0300

    commit

---
+++ base.js
@@ -0,0 +1,34 @@
+const getUsersButton = document.getElementById('submit-data');
+
+getUsersButton.addEventListener('click', function() {
+    const query = document.getElementById('input-show').value;
+    const url = `https://api.tvmaze.com/search/shows?q=${query}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            displayShow(data);
+        })
+});
+
+function displayShow(data) {
+    const container = document.querySelector('.show-container');
+    container.innerHTML = '';
+
+    data.forEach(data => {
+        const show = data.show;
+        const element = document.createElement('div');
+        element.classList.add('show-data');
+
+        const image = show.image ? show.image.medium : '';
+
+        element.innerHTML = `
+            <img src="${image}" alt="${show.name}">
+            <div class="show-info">
+                <h1>${show.name}</h1>
+                ${show.summary}
+            </div>
+        `;
+
+        container.appendChild(element);
+    });
+}
